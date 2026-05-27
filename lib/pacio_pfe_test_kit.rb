14ddd8d34945c1dd3797a3fa995e7a56b6c44e6f
Yunwei Wang <>
2026-05-27 14:12:55 -0500

    first build

--- lib/pacio_pfe_test_kit.rb
+++ lib/pacio_pfe_test_kit.rb
@@ -1,1 +1,2 @@
 require_relative 'pacio_pfe_test_kit/generated/v2.0.0/pfe_test_suite'
+require_relative 'pacio_pfe_test_kit/generated/v3.0.0/pfe_test_suite'
---
+++ lib/pacio_pfe_test_kit/generated/v3.0.0/goal/goal_read_test.rb
@@ -0,0 +1,26 @@
+require_relative '../../../read_test'
+
+module PacioPFETestKit
+  module PacioPFEV300
+    class GoalReadTest < Inferno::Test
+      include PacioPFETestKit::ReadTest
+
+      title 'Server returns correct Goal resource from Goal read interaction'
+      description 'A server SHOULD support the Goal read interaction.'
+
+      id :pfe_v300_goal_read_test
+
+      def resource_type
+        'Goal'
+      end
+
+      def scratch_resources
+        scratch[:goal_resources] ||= {}
+      end
+
+      run do
+        perform_read_test(all_scratch_resources)
+      end
+    end
+  end
+end
